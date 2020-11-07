627d3bf1e384bab1f9ea500b50ac8a1b3c571457
saikumar088 <saikumar.088@gmail.com>
2020-11-07 15:55:23 +0530

    add new gittest.java file

---
+++ src/test/java/com/crm/qa/testcases/gittest.java
@@ -0,0 +1,12 @@
+package com.crm.qa.testcases;
+
+public class gittest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("GIT TESTING first push");
+
+	}
+
+}
